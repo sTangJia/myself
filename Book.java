0f5db46b7af125874d241c97c7426ff961075d8e
sTangJia <2244176086@qq.com>
2019-04-24 16:01:31 +0800

    first commit

---
+++ Book.java
@@ -0,0 +1,16 @@
+public class Book{
+private String name;
+  public String Getname(){
+ int id=0;
+ Setname(String name);
+}
+  private void Setname(String name)
+{
+this.name=name;
+}
+public static void main(){
+  Book book=new Book();
+   System.out.println(book.id);
+    System.out.println(book.Setname("JAVA"));
+}
+}
